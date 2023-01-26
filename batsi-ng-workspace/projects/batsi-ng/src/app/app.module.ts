b6943bd559931b4c36e408f265ed03d75304ff32
Felix Wagner <felix.wagner@outlook.at>
2023-01-26 16:56:02 +0100

    refactor(setup): #4 move header and footer to module

--- batsi-ng-workspace/projects/batsi-ng/src/app/app.module.ts
+++ batsi-ng-workspace/projects/batsi-ng/src/app/app.module.ts
@@ -1,14 +1,12 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { HeaderComponent } from './components/header/header.component';
-import { FooterComponent } from './components/footer/footer.component';
+import { ComponentsModule } from './components/components.module';
 
 @NgModule({
-  declarations: [AppComponent, HeaderComponent, FooterComponent],
-  imports: [BrowserModule, AppRoutingModule],
+  declarations: [AppComponent],
+  imports: [BrowserModule, AppRoutingModule, ComponentsModule],
   providers: [],
   bootstrap: [AppComponent]
 })
