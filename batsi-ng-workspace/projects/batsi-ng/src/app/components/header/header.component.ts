741285e0fb1a686f72c118c4c2a30d15261ddf79
Felix Wagner <felix.wagner@outlook.at>
2023-01-26 16:53:01 +0100

    feat(setup): #4 basic site layout

---
+++ batsi-ng-workspace/projects/batsi-ng/src/app/components/header/header.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'batsi-ng-header',
+  templateUrl: './header.component.html',
+  styleUrls: ['./header.component.scss']
+})
+export class HeaderComponent {}
