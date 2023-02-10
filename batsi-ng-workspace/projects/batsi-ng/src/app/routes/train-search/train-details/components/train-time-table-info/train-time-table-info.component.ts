3e91075e62cc557d527e1519859cd705bda918c2
Felix Wagner <fwagner@mailbox.org>
2023-02-10 19:24:32 +0100

    feat(train-detail): #7 style platform and times

--- batsi-ng-workspace/projects/batsi-ng/src/app/routes/train-search/train-details/components/train-time-table-info/train-time-table-info.component.ts
+++ batsi-ng-workspace/projects/batsi-ng/src/app/routes/train-search/train-details/components/train-time-table-info/train-time-table-info.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 import { TimeTableInfo } from 'batsi-models';
 
 @Component({
@@ -6,6 +6,14 @@ import { TimeTableInfo } from 'batsi-models';
   templateUrl: './train-time-table-info.component.html',
   styleUrls: ['./train-time-table-info.component.scss']
 })
-export class TrainTimeTableInfoComponent {
+export class TrainTimeTableInfoComponent implements OnInit {
   @Input() timeTableInfo: TimeTableInfo | undefined;
+
+  ngOnInit(): void {
+    if (!this.timeTableInfo?.platform) {
+      return;
+    }
+    this.timeTableInfo.platform.reported = 8;
+    this.timeTableInfo.platform.scheduled = void 0;
+  }
 }
