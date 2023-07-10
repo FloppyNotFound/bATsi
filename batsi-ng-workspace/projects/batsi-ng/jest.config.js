65cfaa1801e92e3adab7e29ca92b7818d4754c0f
Felix Wagner <felix.wagner@outlook.at>
2023-07-10 21:02:24 +0200

    feat(ng16): #30 fix tests

--- batsi-ng-workspace/projects/batsi-ng/jest.config.js
+++ batsi-ng-workspace/projects/batsi-ng/jest.config.js
@@ -5,12 +5,15 @@ module.exports = {
   preset: 'jest-preset-angular',
   globals: {
     'ts-jest': {
+      // does currently not work https://github.com/just-jeb/angular-builders/issues/1408
+      // move tsconfig.spec.json back to projects/batsi-ng/, once solved
       tsconfig: '<rootDir>/projects/batsi-ng/tsconfig.spec.json'
     }
   },
   moduleNameMapper: {
     '^(\\.{1,2}/.*)\\.js$': '$1',
-    'batsi-models': '<rootDir>/projects/batsi-models/src/lib/typescript-client'
+    'batsi-models': '<rootDir>/projects/batsi-models/src/lib/typescript-client',
+    'dayjs/esm': 'dayjs'
   },
   modulePathIgnorePatterns: ['<rootDir>/dist']
 };
